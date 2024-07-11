d1ebd151d6802b14e44b54f23aada0591f36fe6f
aminenurgynk <amine.gynk@gmail.com>
2024-07-11 16:59:29 +0300

    created second element -> mesh (object)

--- 01_HelloCube/script.js
+++ 01_HelloCube/script.js
@@ -10,4 +10,23 @@ const scene = new THREE.Scene()
 console.log(scene)
 
 
+// Object is Mesh -->  https://threejs.org/docs/index.html#api/en/objects/Mesh
+// fist thing to do define the shape (geometry) , and the second thing descripe to material
+
+/*
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+const mesh = new THREE.Mesh( geometry, material );
+scene.add( mesh );
+*/ 
+
+
+// Mesh__________________________________________________________________________________________________________________________
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({color:"orange"})
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+
 
