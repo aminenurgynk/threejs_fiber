afb500916bef6dab6685d814de156b81883c79fd
aminenurgynk <amine.gynk@gmail.com>
2024-07-11 16:58:56 +0300

    created first element scene

--- 01_HelloCube/script.js
+++ 01_HelloCube/script.js
@@ -1,3 +1,13 @@
-/*import * as THREE from "./three.module.min.js"
-console.log(THREE)*/
+import * as THREE from "./three.module.min.js"
+console.log(THREE)
+
+// We have 4 elements which are
+// Scene Mesh(Object) Camera Renderer
+
+// Scene_________________________________________________________________________________________________________________________
+
+const scene = new THREE.Scene()
+console.log(scene)
+
+
 
